bbfecf7c7c74d79ce9aea813a6042f2153ff4e0a
jin_sin <jsin@appknot.com>
2019-06-10 11:56:26 +0900

    [add] - Rx 관련 Extensions 추가 - lifecycle 를 통한 Disposable 관리 소스 추가

---
+++ app/src/main/java/com/example/seotda/extensions/LifecycleExtensions.kt
@@ -0,0 +1,11 @@
+package com.example.seotda.extensions
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+
+/**
+ *
+ * @author Jin on 2019-06-10
+ */
+
+operator fun Lifecycle.plusAssign(observer: LifecycleObserver) = this.addObserver(observer)
---
+++ app/src/main/java/com/example/seotda/extensions/RxJavaExtensions.kt
@@ -0,0 +1,17 @@
+package com.example.seotda.extensions
+
+import com.example.seotda.rx.AutoClearedDisposable
+import io.reactivex.Completable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+/**
+ *
+ * @author Jin on 2019-06-10
+ */
+
+operator fun AutoClearedDisposable.plusAssign(disposable: Disposable)
+        = this.add(disposable)
+
+fun runOnIoScheduler(func: () -> Unit): Disposable
+        = Completable.fromCallable(func).subscribeOn(Schedulers.io()).subscribe()
